dfcfff1951084aea09aed826ad2585bcf550e8f0
DRouh <dimerouh@gmail.com>
2017-03-26 17:42:20 +0300

    Add chapter 3 tests

---
+++ akkademaid/src/test/scala/Articles.scala
@@ -0,0 +1,240 @@
+
+object Articles {
+  val article1 =
+    """
+      |<!DOCTYPE html>
+      |<html>
+      |  <head>
+      |
+      |    <title>Documentation | The Scala Programming Language</title>
+      |
+      |    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+      |
+      |    <link rel="icon" type="image/png" href="/resources/favicon.ico">
+      |    <link rel="shortcut icon" type="image/png" href="/resources/favicon.ico">
+      |
+      |    <!-- prettify CSS (corresponding js at footer)-->
+      |    <link rel="stylesheet" href="/resources/css/prettify.css" type="text/css" />
+      |
+      |    <!-- Bootstrap CSS -->
+      |    <link rel="stylesheet" href="/resources/css/bootstrap.css" type="text/css" />
+      |
+      |    <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700,900,400italic,700italic' rel='stylesheet' type='text/css'>
+      |
+      |    <!-- Custom stylesheet -->
+      |    <link rel="stylesheet" href="/resources/css/main.css" type="text/css" />
+      |
+      |    <!-- Typekit (should stay at top of page, do not move to footer)-->
+      |    <script type="text/javascript" src="//use.typekit.net/abh3wgk.js"></script>
+      |    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+      |
+      |    <!-- Atom feeds -->
+      |    <link rel="alternate" type="application/atom+xml" title="News Feed" href="/feed/index.xml" />
+      |    <link rel="alternate" type="application/atom+xml" title="Blog Feed" href="/feed/blog.xml" />
+      |
+      |
+      |  </head>
+      |  <body>
+      |
+      |
+      |<div class="darkstrip"></div>
+      |
+      |<div class="darkbluetopbar">
+      |  <div class="container" style="height: 100%">
+      |    <div class="navbar-wrap">
+      |      <div class="container" style="position: relative; height: 100%;">
+      |        <div class="scala-logo">
+      |          <a href="/index.html"><img src="/resources/img/scala-logo-white-sm.png" alt="white Scala logo" /></a>
+      |        </div>
+      |        <ul class="nav">
+      |  <li><a href="/documentation/">Documentation</a></li>
+      |  <li><a href="/download/">Download</a></li>
+      |  <li><a href="/community/">Community</a></li>
+      |  <li><a href="/contribute/">Contribute</a></li>
+      |  <li id="source-code" >
+      |    <a href="http://github.com/scala/scala"><img src="/resources/img/github-logo.png" alt="GitHub Logo" /></a>
+      |    <div class="toptip">Source Code</div>
+      |  </li>
+      |  <li id="scala-lang-twitter">
+      |    <a href="http://twitter.com/scala_lang"><img src="/resources/img/twitter-logo-white.png" alt="Twitter Logo" /></a>
+      |    <div class="toptip">Scala on Twitter</div>
+      |  </li>
+      |</ul>
+      |
+      |      </div>
+      |    </div>
+      |  </div>
+      |</div>
+      |
+      |<div class="midbluebar">
+      |  <div class="container">
+      |    <h1 id="page-title">Documentation</h1>
+      |  </div>
+      |</div>
+      |
+      |<div class="page-container">
+      |  <div class="container" style="min-height: 540px;">
+      |    <div class="main-page-column">
+      |
+      |  <div class="container">
+      |  <div class="row">
+      |    <div class="span4 doc-block">
+      |      <h3><a href="/documentation/getting-started.html">Getting Started</a></h3>
+      |      <p>Install Scala on your computer and start writing some Scala code!</p>
+      |    </div>
+      |    <div class="span4 doc-block">
+      |      <h3><a href="http://docs.scala-lang.org/overviews/">Overviews/Guides</a></h3>
+      |      <p>Access detailed documentation on important language features.</p>
+      |    </div>
+      |  </div>
+      |
+      |  <div class="row">
+      |    <div class="span4 doc-block">
+      |      <h3><a href="/documentation/api.html">API</a></h3>
+      |      <p>Dive straight into the API.</p>
+      |    </div>
+      |    <div class="span4 doc-block">
+      |      <h3><a href="/files/archive/spec/2.12/">Specification</a></h3>
+      |      <p>Get an in-depth overview of the language.</p>
+      |    </div>
+      |  </div>
+      |
+      |  <div class="row">
+      |    <div class="span4 doc-block">
+      |      <h3><a href="http://docs.scala-lang.org/cheatsheets/">Cheatsheets</a></h3>
+      |      <p>Access language constructs quickly.</p>
+      |    </div>
+      |    <div class="span4 doc-block">
+      |      <h3><a href="http://docs.scala-lang.org/style/">Style Guide</a></h3>
+      |      <p>Learn how to code elegantly.</p>
+      |    </div>
+      |
+      |  </div>
+      |
+      |  <div class="row">
+      |    <div class="span4 doc-block">
+      |      <h3><a href="http://docs.scala-lang.org/tutorials/">Tutorials</a></h3>
+      |      <p>Digest bite-size pieces of the essentials.</p>
+      |    </div>
+      |    <div class="span4 doc-block">
+      |      <h3><a href="http://docs.scala-lang.org/glossary/">Glossary</a></h3>
+      |      <p>Understand Scala's vocabulary.</p>
+      |    </div>
+      |  </div>
+      |
+      |</div>
+      |
+      |<h2 id="online-learning">Online Learning</h2>
+      |
+      |<p>There are a few interactive resources for trying out Scala, to get a look and feel of the language:</p>
+      |
+      |<ul>
+      |  <li><a href="https://www.coursera.org/course/progfun">Functional Programming Principles in Scala</a>, free on Coursera. This is a course about functional programming given by Martin Odersky himself. You can access the course material and exercises by
+      | signing up for the course.</li>
+      |  <li><a href="https://www.coursera.org/specializations/scala">Functional Programming in Scala Specialization</a>, free on Coursera. The Specialization provides a hands-on introduction to functional programming using Scala. You can access the courses material and exercises by signing up for the specialization. Includes the following courses:
+      |    <ul>
+      |      <li>Functional Programming Principles in Scala</li>
+      |      <li>Functional Program Design in Scala</li>
+      |      <li>Parallel programming</li>
+      |      <li>Big Data Analysis with Scala and Spark</li>
+      |      <li>Functional Programming in Scala Capstone</li>
+      |    </ul>
+      |  </li>
+      |  <li><strong>Try Scala In Your Browser!</strong>: There are a handful of websites where you can interactively run Scala code in your browser! Have a look at <a href="http://www.scala-js-fiddle.com/">scala-js-fiddle</a>, <a href="http://www.scalakata.com/">Scala Kata</a>, and <a href="http://scastie.org/">Scastie</a> for more.</li>
+      |  <li><a href="http://getscala.com">Independent Courseware</a>, online self-study or instructor-led Scala and Play courses for a fee.</li>
+      |</ul>
+      |
+      |<h2 id="books">Books</h2>
+      |
+      |<p>There are more and more books being published about Scala. <a href="/documentation/books.html">Here</a>, you can find just a small selection of the many available titles.</p>
+      |
+      |<h2 id="bleeding-edge">Bleeding Edge</h2>
+      |
+      |<p>If you are interested in finding out about the hottest, most pressing issues of tomorrow in the Scala world, have a look at the
+      |<a href="http://docs.scala-lang.org/sips/">Scala Improvement Process (SIP) page</a>.</p>
+      |
+      |<h2 id="older-documentation">Older Documentation</h2>
+      |
+      |<p>The documentation below may be a bit outdated, but provides insights into the (historical) design principles of
+      |the language :</p>
+      |
+      |<ul>
+      |  <li><a href="http://www.scala-lang.org/docu/files/ScalaTutorial.pdf">Brief Scala Tutorial</a>: a 20 page introduction to scala and some of the basic concepts and a good place to start. You will find more code examples <a href="http://www.scala-lang.org/node/219">here</a>.</li>
+      |  <li><a href="http://www.scala-lang.org/docu/files/ScalaByExample.pdf">Scala By Example</a>: takes you through the Scala features with many examples. It does assume that you are already familiar with the basic Scala syntax and a basic understanding of functional programming. It is an excellent way to expand your knowledge and skill.</li>
+      |  <li><a href="http://www.scala-lang.org/docu/files/ScalaOverview.pdf">Scala Overview</a>: this is a paper summarizing the features of the Scala Language in a formal and concise way. An excellent reference for language researchers or advanced programmers.</li>
+      |  <li><a href="http://www.scala-lang.org/node/960">Java to Scala with the Help of Experts</a>: a collection of some of the almost endless supply of tips available for Java programmers new to Scala. There are also mini-blog series designed to take you through many of the important features of the Scala language in a friendly way.</li>
+      |</ul>
+      |
+      |</div>
+      |<div class="sidebar">
+      |  <div class="sidebar-inner">
+      |    <div class="contents">Contents</div>
+      |    <div id="toc"></div>
+      |    <br/>
+      |    <div class="alert-info">
+      |      <a href="/contribute/documentation.html#updating-scala-langorg"><p><strong>Problem with this page?</strong>
+      |      Please help us fix it!</p></a>
+      |    </div>
+      |  </div>
+      |</div>
+      |<div style="clear:both"></div>
+      |
+      |  </div>
+      |</div>
+      |
+      |
+      |  <div class="footer">
+      |  <div class="container">
+      |    <ul>
+      |      <li><h5>Documentation</h5></li>
+      |      <li><a href="/documentation/getting-started.html">Getting Started</a></li>
+      |      <li><a href="http://www.scala-lang.org/api/current/index.html#package">API</a></li>
+      |      <li><a href="http://docs.scala-lang.org/overviews/">Overviews/Guides</a></li>
+      |      <li><a href="http://docs.scala-lang.org/tutorials/">Tutorials</a></li>
+      |      <li><a href="/files/archive/spec/2.12/">Language Specification</a></li>
+      |    </ul>
+      |    <ul>
+      |      <li><h5>Download</h5></li>
+      |        <li><a href="/download/">Current Version</a></li>
+      |	<li><a href="/download/all.html">All Versions</a></li>
+      |    </ul>
+      |    <ul>
+      |      <li><h5>Community</h5></li>
+      |      <li><a href="/community/">Community</a></li>
+      |      <li><a href="/community/index.html#mailing-lists">Mailing Lists</a></li>
+      |      <li><a href="/community/index.html#chat-rooms">Chat Rooms &amp; More</a></li>
+      |      <li><a href="/community/index.html#community-libraries-and-tools">Libraries and Tools</a></li>
+      |      <li><a href="https://scala.epfl.ch/">The Scala Center</a></li>
+      |    </ul>
+      |    <ul>
+      |      <li><h5>Contribute</h5></li>
+      |      <li><a href="/contribute">How to Help</a></li>
+      |      <li><a href="/contribute/bug-reporting-guide.html">Report an Issue</a></li>
+      |    </ul>
+      |    <ul>
+      |      <li><h5>Scala</h5></li>
+      |      <li><a href="/blog">Blog</a></li>
+      |      <li><a href="/news">Archive</a></li>
+      |      <li><a href="/conduct.html">Code of Conduct</a></li>
+      |      <li><a href="/license.html">License</a></li>
+      |    </ul>
+      |    </div>
+      |    <div class="container">
+      |      <div class="copyright">
+      |        <a style="float: left; margin-right: 8px;" href="http://www.epfl.ch"><img src="/resources/img/epfl-logo.png" alt="EPFL logo" /></a>
+      |        <p style="float: left;"><br/>Copyright &copy; 2002-<span class="current-year"></span> École Polytechnique Fédérale de Lausanne
+      |        (EPFL)<br/> Lausanne, Switzerland</p>
+      |      </div>
+      |      <div class="scala-logo-footer">
+      |        <img src="/resources/img/scala-logo-red-footer.png" alt="Scala footer logo" />
+      |      </div>
+      |    </div> <!-- container -->
+      |    </div> <!-- footer -->
+      |
+      |    <!-- Custom javascript -->
+      |    <script src="/resources/js/main.js" type="text/javascript"></script>
+      |  </body>
+      |</html>
+      |
+      |""".stripMargin
+}
--- akkademaid/src/test/scala/AskAndTellDemoSpec.scala
+++ akkademaid/src/test/scala/AskAndTellDemoSpec.scala
@@ -1,8 +1,14 @@
+import akka.actor.Status.Failure
 import akka.actor.{ActorSystem, Props}
+import akka.pattern.ask
 import akka.testkit.TestProbe
 import akka.util.Timeout
+import com.akkademy.messages.{GetRequest, SetRequest}
 import com.akkademy._
+import de.l3s.boilerpipe.extractors.ArticleExtractor
 import org.scalatest.{FunSpec, Matchers}
+
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 
 class AskAndTellDemoSpec extends FunSpec with Matchers {
@@ -21,17 +27,71 @@ class AskAndTellDemoSpec extends FunSpec with Matchers {
         timeout)
     )
 
-    describe("tell demo") {
-      val cacheProbe = TestProbe()
-      val httpClientProbe = TestProbe()
-      val articleParseActor = system.actorOf(Props[ParsingActor])
-      val tellDemoActor = system.actorOf(
-        Props(classOf[TellDemoArticleParser],
-          cacheProbe.ref.path.toString,
-          httpClientProbe.ref.path.toString,
-          articleParseActor.path.toString,
-          timeout)
-      )
+    it("should provide parsed article") {
+      val f = askDemoActor ? ParseArticle("http://www.google.com")
+
+      cacheProbe.expectMsgType[GetRequest]
+      cacheProbe.reply(Failure(new Exception("no cache")))
+
+      httpClientProbe.expectMsgType[String]
+      httpClientProbe.reply(HttpResponse(Articles.article1))
+
+      cacheProbe.expectMsgType[SetRequest]
+
+      val parsedArticle = Await.result(f, 10 seconds)
+      parsedArticle.toString should include("Scala By Example")
+      parsedArticle.toString should not include("<body>")
+    }
+
+    it("should provide cached article") {
+      val f = askDemoActor ? ParseArticle("http://www.google.com")
+
+      cacheProbe.expectMsgType[GetRequest]
+      cacheProbe.reply(ArticleExtractor.INSTANCE.getText(Articles.article1))
+
+      val parsedArticle = Await.result(f, 10 seconds)
+      parsedArticle.toString should include("Scala By Example")
+      parsedArticle.toString should not include("<body>")
     }
   }
+
+  describe("tell demo") {
+    val cacheProbe = TestProbe()
+    val httpClientProbe = TestProbe()
+    val articleParseActor = system.actorOf(Props[ParsingActor])
+    val tellDemoActor = system.actorOf(
+      Props(classOf[TellDemoArticleParser],
+        cacheProbe.ref.path.toString,
+        httpClientProbe.ref.path.toString,
+        articleParseActor.path.toString,
+        timeout)
+    )
+    it("should provide parsed article") {
+      val f = tellDemoActor ? ParseArticle("http://www.google.com")
+
+      cacheProbe.expectMsgType[GetRequest]
+      cacheProbe.reply(Failure(new Exception("no cache")))
+
+      httpClientProbe.expectMsgType[String]
+      httpClientProbe.reply(HttpResponse(Articles.article1))
+
+      cacheProbe.expectMsgType[SetRequest]
+
+      val parsedArticle = Await.result(f, 10 seconds)
+      parsedArticle.toString should include("Scala By Example")
+      parsedArticle.toString should not include("<body>")
+    }
+
+    it("should provide cached article") {
+      val f: Future[Any] = tellDemoActor ? ParseArticle("http://www.google.com")
+
+      cacheProbe.expectMsgType[GetRequest]
+      cacheProbe.reply(ArticleExtractor.INSTANCE.getText(Articles.article1))
+
+      val parsedArticle = Await.result(f, 10 seconds)
+      parsedArticle.toString should include("Scala By Example")
+      parsedArticle.toString should not include("<body>")
+    }
+  }
+
 }
